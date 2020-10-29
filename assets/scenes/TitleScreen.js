4a82dd243c84176786e7046fd1ba53ca55bb0fbb
Arian Fornaris <developers@phasereditor2d.com>
2020-10-29 01:27:28 -0400

    Listen pointerdown in the title screen.

--- assets/scenes/TitleScreen.js
+++ assets/scenes/TitleScreen.js
@@ -4,10 +4,10 @@
 /* START OF COMPILED CODE */
 
 class TitleScreen extends Phaser.Scene {
-	
+
 	constructor() {
 		super("TitleScreen");
-		
+
 		/** @type {Phaser.GameObjects.TileSprite} */
 		this.background;
 		/** @type {Phaser.GameObjects.TileSprite} */
@@ -18,63 +18,66 @@ class TitleScreen extends Phaser.Scene {
 		this.press_enter_text;
 		/** @type {Phaser.GameObjects.Image} */
 		this.instructions;
-		
+
 		/* START-USER-CTR-CODE */
 		// Write your code here.
 		/* END-USER-CTR-CODE */
 	}
-	
+
 	editorCreate() {
-		
+
 		// background
 		const background = this.add.tileSprite(0, 0, 384, 240, "back");
 		background.setOrigin(0, 0);
-		
+
 		// middle
 		const middle = this.add.tileSprite(0, 80, 384, 368, "middle");
 		middle.setOrigin(0, 0);
-		
+
 		// title_screen
 		const title_screen = this.add.image(144, 90, "title-screen");
-		
+
 		// credits_text
 		this.add.image(144, 174, "credits-text");
-		
+
 		// press_enter_text
 		const press_enter_text = this.add.image(144, 149, "press-enter-text");
-		
+
 		// instructions
 		const instructions = this.add.image(144, 0, "instructions");
 		instructions.setOrigin(0.5, 0);
 		instructions.visible = false;
-		
+
 		this.background = background;
 		this.middle = middle;
 		this.title_screen = title_screen;
 		this.press_enter_text = press_enter_text;
 		this.instructions = instructions;
 	}
-	
+
 	/* START-USER-CODE */
 
 	create() {
 
 		this.editorCreate();
 
-		this.input.keyboard.on("keydown_ENTER", () => {
-
-			if (this.title_screen.visible) {
-
-				this.title_screen.visible = false;
-				this.instructions.visible = true;
-				
-			} else {
-
-				this.scene.start("Level");
-			}			
-		});		
+		this.input.keyboard.on("keydown_ENTER", this.enterPressed, this);
+		this.input.on("pointerdown", this.enterPressed, this);
 
 		this.blinkText();
+	}
+
+	enterPressed() {
+		
+		if (this.title_screen.visible) {
+
+			this.title_screen.visible = false;
+			this.instructions.visible = true;
+
+		} else {
+
+			this.scene.start("Level");
+		}
 	}
 
 	blinkText() {
@@ -85,11 +88,11 @@ class TitleScreen extends Phaser.Scene {
 			callback: () => {
 				this.press_enter_text.visible = !this.press_enter_text.visible;
 			}
-		});		
+		});
 	}
 
 	update() {
-		
+
 		this.background.tilePositionX += 0.3;
 		this.middle.tilePositionX += 0.6;
 	}
