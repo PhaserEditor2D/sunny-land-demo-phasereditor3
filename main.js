69fcc85482c5a6113005d542a11de8a724765352
Arian Fornaris <developers@phasereditor2d.com>
2020-10-28 23:37:20 -0400

    Start in fullscreen.

--- main.js
+++ main.js
@@ -26,6 +26,10 @@ window.addEventListener('load', function () {
 	
 	game.scene.add("Boot", Boot, true);
 
+	document.body.addEventListener("pointerdown", () => {
+
+		game.scale.startFullscreen();
+	});
 });
 
 class Boot extends Phaser.Scene {
